2adf89bfa831cb2d1a4ff5aba9c868e1cd9222b9
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-11-13 08:56:56 -0600

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Krow on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
